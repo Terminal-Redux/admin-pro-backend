dc5fd8b12fcd59b0d1b92fb22b2d98ac3bde3d9c
Amilcar Gonzalez <amilkargm@gmail.com>
2022-12-22 20:51:38 -0600

    SPA control

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 
 const express = require('express');
 const cors = require('cors');
@@ -28,7 +29,10 @@ app.use('/api/login', require('./routes/auth'));
 app.use('/api/todo', require('./routes/busquedas'));
 app.use('/api/upload', require('./routes/uploads'));
 
-
+// lo último
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+})
 
 
 
